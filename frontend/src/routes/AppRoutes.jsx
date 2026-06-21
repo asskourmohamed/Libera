247b2abc89067cec2ce37e6c5b67e4834912f074
ASSKOUR <asskourmohamed1@gmail.com>
2026-06-21 23:12:08 +0100

    adding frontend

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,7 +4,7 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Explore from "../pages/Explore";
-//somethingEFFqueKADDO
+//somethingEFFque
 function AppRoutes() {
   return (
     <Routes>
